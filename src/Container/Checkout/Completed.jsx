46736130607fb9e98c6cc22b345a1ab4664f6a68
fadhilansyah25 <fadhilansyah25@gmail.com>
2021-02-17 16:06:48 +0700

    Pembuatan Container Checkout Comploted

--- src/Container/Checkout/Completed.jsx
+++ src/Container/Checkout/Completed.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Fade from 'react-reveal/Fade'
-import CompIllust from '../../Assets/Images/Completed.svg'
+import React from "react";
+import Fade from "react-reveal/Fade";
+import CompIllustration from "../../Assets/Images/Completed.svg";
 
 export default function Completed() {
-    return (
-        <div>
-            
+  return (
+    <Fade>
+      <div className="container" style={{ marginBottom: 30 }}>
+        <div className="row justify-content-center text-center">
+          <div className="col-4">
+            <img src={CompIllustration} alt="completed checkout illustration" />
+            <p className="text-gray-500">
+              We will inform you via email latter once the transaction has been
+              accepted
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </Fade>
+  );
 }
