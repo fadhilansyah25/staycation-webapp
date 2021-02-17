ec05ca805616377eae0acfad767aa44c9ee10749
fadhilansyah25 <fadhilansyah25@gmail.com>
2021-02-17 14:54:16 +0700

    Pembuatan Container Payment

---
+++ src/Container/Checkout/Completed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade'
+import CompIllust from '../../Assets/Images/Completed.svg'
+
+export default function Completed() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
